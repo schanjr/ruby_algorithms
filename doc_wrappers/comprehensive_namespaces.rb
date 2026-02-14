9c4eca632806c2371ab7f6fe93e333a66de0bad4
Stanley <schanjr@users.noreply.github.com>
2026-02-14 11:05:00 -0600

    Update documentation and namespaces - Feb 14 2026

--- doc_wrappers/comprehensive_namespaces.rb
+++ doc_wrappers/comprehensive_namespaces.rb
@@ -4996,20 +4996,6 @@ module PopularAlgorithms
         end
       end
 
-      atlanta = City.new('Atlanta')
-      boston = City.new('Boston')
-      chicago = City.new('Chicago')
-      denver = City.new('Denver')
-      el_paso = City.new('El Paso')
-
-      atlanta.add_route(boston, 100)
-      atlanta.add_route(denver, 160)
-      boston.add_route(chicago, 120)
-      boston.add_route(denver, 180)
-      chicago.add_route(el_paso, 80)
-      denver.add_route(chicago, 40)
-      denver.add_route(el_paso, 140)
-
       def dijkstra(starting_city, other_cities)
         # The routes_from_city hash table below holds the data of all price_infos
         # from the given city to all other destinations, and the city which it
@@ -5054,9 +5040,9 @@ module PopularAlgorithms
           current_city.routes.each do |city, price_info|
             # If the route from the starting city to the other city
             # is cheaper than currently recorded in routes_from_city, we update it:
-            next unless routes_from_city[city][0] > price_info + routes_from_city[current_city][0]
-
-            routes_from_city[city] = [price_info + routes_from_city[current_city][0], current_city]
+            if routes_from_city[city][0] > price_info + routes_from_city[current_city][0]
+              routes_from_city[city] = [price_info + routes_from_city[current_city][0], current_city]
+            end
           end
 
           # We determine which city to visit next:
@@ -5066,10 +5052,10 @@ module PopularAlgorithms
           routes_from_city.each do |city, price_info|
             # if this route is the cheapest from this city, and it has not yet been
             # visited, it should be marked as the city we'll visit next:
-            next unless price_info[0] < cheapest_route_from_current_city && !visited_cities.include?(city)
-
-            cheapest_route_from_current_city = price_info[0]
-            current_city = city
+            if price_info[0] < cheapest_route_from_current_city && !visited_cities.include?(city)
+              cheapest_route_from_current_city = price_info[0]
+              current_city = city
+            end
           end
 
         end
@@ -5077,9 +5063,23 @@ module PopularAlgorithms
         routes_from_city
       end
 
+      atlanta = City.new('Atlanta')
+      boston = City.new('Boston')
+      chicago = City.new('Chicago')
+      denver = City.new('Denver')
+      el_paso = City.new('El Paso')
+
+      atlanta.add_route(boston, 100)
+      atlanta.add_route(denver, 160)
+      boston.add_route(chicago, 120)
+      boston.add_route(denver, 180)
+      chicago.add_route(el_paso, 80)
+      denver.add_route(chicago, 40)
+      denver.add_route(el_paso, 140)
+
       routes = dijkstra(atlanta, [boston, chicago, denver, el_paso])
       routes.each do |city, price_info|
-        p "#{city.name}: #{price_info[0]}"
+        p "Shortest Path to city: #{city.name} is: #{price_info[0]}"
       end
 
     end
